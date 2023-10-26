2eda23264bb1b0e0335e8380b8070f90ee251ba0
JTeo99 <>
2023-10-26 13:23:42 +0000

    intial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Setting constants within the game
+# Ensuring tha the maximum screen occupancy of the pygame is within the specified limits
+SCREEN_WIDTH, SCREEN_HEIGHT = 80, 24
+# Initial set grid size to ensure game works
+GRID_SIZE = 5
+# Allowing game to take up the whole size of the Pygame Window
+CELL_SIZE = SCREEN_WIDTH // GRID_SIZE
+# Setting the colours within the game for consistency
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
