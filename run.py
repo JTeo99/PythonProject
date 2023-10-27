087dcb9f13e8229544adcd72e081b94e83fb144f
JTeo99 <>
2023-10-27 05:22:38 +0000

    inline comments made to clarify code

--- run.py
+++ run.py
@@ -14,12 +14,19 @@ RESET = Style.RESET_ALL
 
 
 def clear_terminal():
+    """
+    Function to get user input for the grid size. Prompts the user for the
+    number of rows and columns, ensuring they are between 2 and 6.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
     print("\033[H")
 
 
-# Function to get user input for the grid size
-def get_grid_size():
+def get_grid_size():  # Function to get user input for the grid size
+    """
+    Function to get user input for the grid size. Prompts the user for the
+    number of rows and columns, ensuring they are between 2 and 6.
+    """
     while True:
         try:
             rows = int(input("Enter the number of rows (2-6): "))
@@ -32,76 +39,96 @@ def get_grid_size():
             print("Invalid input. Please enter valid integers.")
 
 
-board_size = get_grid_size()
-
-
-# Initialize the player and computer game boards with labels
-def reset_board():
+def reset_board(board_size):
+    """
+    Initialize the game boards
+    """
     player_board = [[" " for _ in range(board_size[1])]
                     for _ in range(board_size[0])]
-    computer_board = [[" " for _ in range(board_size[1])]
-                      for _ in range(board_size[0])]
-    return player_board, computer_board
+    comp_board = [[" " for _ in range(board_size[1])]
+                  for _ in range(board_size[0])]
+    return player_board, comp_board
 
 
-# Initialize the player and computer ship positions
-player_board, computer_board = reset_board()
-player_ship = [(random.randint(0, board_size[0] - 1),
-                random.randint(0, board_size[1] - 1))]
-computer_ship = [(random.randint(0, board_size[0] - 1),
-                  random.randint(0, board_size[1] - 1))]
-
-
-# Function to print messages in color
-def print_message(message, color=RESET):
+def print_message(message, color=RESET):  # Function to print messages in color
     print(color + message + RESET)
 
 
-# Function to clear the current line in the terminal
-def clear_line():
+def clear_line():  # Function to clear the current line in the terminal
     sys.stdout.write("\033[K")
 
 
-# Function to generate the board with coordinates and labels
-def generate_coordinates(board):
+def generate_coordinates(board, board_size):
+    """
+    Function to generate the game board with coordinates and labels. The
+    coordinates include row numbers and column letters. This function
+    creates the top row labels.
+    """
     header = [""]
     header += [chr(ord('A') + i) for i in range(board_size[1])]
     header += [""]
     result = [header]
 
+    # Generating rows with data and row labels
     for i, row in enumerate(board):
         result.append([str(i + 1)] + row + [str(i + 1)])
-    result.append(header)
     return result
 
 
-# Function to print the game boards with coordinates and labels
-def print_coordinates(player_board, computer_board):
-    player_coordinates = generate_coordinates(player_board)
-    computer_coordinates = generate_coordinates(computer_board)
+def print_coordinates(player_board, comp_board, board_size):
+    """
+    Function to print both game boards with coordinates and labels. It creates
+    the visual representation of the game boards and their labels.
+    """
+    player_coordinates = generate_coordinates(player_board, board_size)
+    comp_coordinates = generate_coordinates(comp_board, board_size)
 
-    print("Player's Grid:")
-    print(tabulate(player_coordinates, tablefmt="grid"))
+    """
+    Prints labels for the Player's side of the grid and Computer's side of the
+    grid. Initializes an empty list to later store the combined grid for the
+    player and the computer.
+    """
 
-    print("Computer's Grid:")
-    print(tabulate(computer_coordinates, tablefmt="grid"))
+    # Print labels above the table
+    print("Player's Grid" + "    " * board_size[1] + "Computer's Grid")
+
+    combined_grid = []
+
+    # Combine player and computer grid rows for display
+    for i in range(max(len(player_coordinates), len(comp_coordinates))):
+        player_row = player_coordinates[i] if i < len(player_coordinates) else [""] * (board_size[1] + 2)
+        comp_row = comp_coordinates[i] if i < len(comp_coordinates) else [""] * (board_size[1] + 2)
+        combined_row = player_row + comp_row
+        combined_grid.append(combined_row)
+
+    print(tabulate(combined_grid, tablefmt="grid"))
 
 
-# Function to get the user's guess (row and column)
-def get_user_guess():
+def get_user_guess(board_size):
+    """
+    Function to get the user's guess (row and column). It ensures that the
+    user's input is correctly formatted and within the game's bounds.
+    """
     while True:
         try:
             guess = input(f"Enter your guess (e.g., A1, max {chr(ord('A') + board_size[1] - 1)}{board_size[0]}): ")
 
             if (guess == "restart"):
-
+                """
+                checks for "restart" as an input to stop the rest of the game
+                and clear the terminal. Time library for 1 second
+                break before 'calling play-battleships()' to rebuild the game.
+                """
                 clear_terminal()
                 time.sleep(1)
                 play_battleships()
 
+            """
+            Converts stored guess to upper case
+            """
             col_row = guess.upper()
 
-            # Convert column letter to zero-based index
+            # Column letter to zero-based index
             col = ord(col_row[0]) - ord('A')
 
             # Subtract 1 to convert from 1-based to 0-based index
@@ -120,34 +147,43 @@ def get_user_guess():
             print("Invalid input. Please enter valid row and column values.")
 
 
-# Function to check if a guess hits a ship
 def check_guess(row, col, target_ship):
+    """
+    Function to check if a guess hits a ship. It checks if the provided row
+    and column match any of the positions in the target ship.
+    """
     if (row, col) in target_ship:
         return True
     return False
 
 
-# Main game loop
 def play_battleships():
+    """
+    Main game loop function. It manages the game logic, including player and
+    computer turns, checking for hits, and asking if the player wants to
+    play again.
+    """
     while True:
-        # Initialize the player and computer boards
-        player_board, computer_board = reset_board()
+
+        board_size = get_grid_size()  # setting board size
+
+        player_board, comp_board = reset_board(board_size)  # reset board
 
         print_message("Welcome to Battleships!", GREEN)
 
         while True:
             # Print both game boards with coordinates and labels
-            print_coordinates(player_board, computer_board)
+            print_coordinates(player_board, comp_board, board_size)
             player_ship = [(random.randint(0, board_size[0] - 1),
                             random.randint(0, board_size[1] - 1))]
-            computer_ship = [(random.randint(0, board_size[0] - 1),
-                              random.randint(0, board_size[1] - 1))]
+            comp_ship = [(random.randint(0, board_size[0] - 1),
+                          random.randint(0, board_size[1] - 1))]
 
             # Player's turn
-            player_row, player_col = get_user_guess()
+            player_row, player_col = get_user_guess(board_size)
 
-            if check_guess(player_row, player_col, computer_ship):
-                print_message("Congratulations! You've sunk the computer's battleship!", GREEN)
+            if check_guess(player_row, player_col, comp_ship):
+                print_message("Wow! You've sunk the enemy battleship!", GREEN)
                 break
 
             elif (player_board[player_row][player_col] == ' '):
@@ -155,28 +191,35 @@ def play_battleships():
                 player_board[player_row][player_col] = 'X'
 
             else:
-                print_message("You already guessed this location.", RED)
+                print_message("You already guess here.", RED)
 
             # Clear the previous board display
             for _ in range(board_size[0] + 5):
                 clear_line()
                 sys.stdout.write("\033[A")
 
-            # Computer's turn
-            computer_row, computer_col = random.randint(0, board_size[0] - 1), random.randint(0, board_size[1] - 1)
-            print_message(f"The computer takes a guess at position: {chr(ord('A') + computer_row)} {computer_col + 1}", GREEN)
+            '''
+            Computer's turn to pick. Randomely selects a set of coordinates
+            within the range of the grid and prints the location by which the
+            computer chose. Player loses if Computer guesses correctly but
+            loops to player pick if computer is incorrect.
+            '''
+            comp_row, comp_col = random.randint(0, board_size[0] - 1), random.randint(0, board_size[1] - 1)
+            print_message(f"The computer takes a guess at position: {chr(ord('A') + comp_row)} {comp_col + 1}", GREEN)
 
-            if check_guess(computer_row, computer_col, player_ship):
-                print_message("The computer has sunk your battleship! You lose.", RED)
+            if check_guess(comp_row, comp_col, player_ship):
+                print_message("Your battleship has sunk! You lose.", RED)
                 break
 
-            elif (computer_board[computer_row][computer_col] == ' '):
+            elif (comp_board[comp_row][comp_col] == ' '):
                 print_message("The computer missed.", RED)
-                computer_board[computer_row][computer_col] = 'X'
+                comp_board[comp_row][comp_col] = 'X'
 
             else:
-                print_message("The computer already guessed this location.", RED)
-
+                print_message("The computer already guessed here.", RED)
+        """
+        restart built into the game
+        """
         restart = input("Do you want to play again? (yes/no): ").strip().lower()
         if restart != 'yes':
             break
