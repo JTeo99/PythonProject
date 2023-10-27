c9b10ab84b79fd60081b6c5012ddb37f91d74795
JTeo99 <>
2023-10-27 01:20:14 +0000

    restart function added

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
+from tabulate import tabulate
+from colorama import Fore, Back, Style, init
+
 import random
 import sys
 import time
-from tabulate import tabulate
 import os
 
-# ANSI color codes
-GREEN = '\033[92m'
-RED = '\033[91m'
-RESET = '\033[0m'
+init(autoreset=True)  # Initializing Colorama
+
+GREEN = Fore.GREEN
+RED = Fore.RED
+RESET = Style.RESET_ALL
 
 
 def clear_terminal():
@@ -34,13 +37,17 @@ max_columns = 80
 max_rows = 24
 board_size = get_grid_size()
 
+
 # Initialize the player and computer game boards with labels
-player_board = [[" " for _ in range(board_size[1])]
-                for _ in range(board_size[0])]
-computer_board = [[" " for _ in range(board_size[1])]
-                  for _ in range(board_size[0])]
+def reset_board():
+    player_board = [[" " for _ in range(board_size[1])]
+                    for _ in range(board_size[0])]
+    computer_board = [[" " for _ in range(board_size[1])]
+                      for _ in range(board_size[0])]
+    return player_board, computer_board
 
 # Initialize the player and computer ship positions
+player_board, computer_board = reset_board()
 player_ship = [(random.randint(0, board_size[0] - 1),
                 random.randint(0, board_size[1] - 1))]
 computer_ship = [(random.randint(0, board_size[0] - 1),
@@ -58,7 +65,7 @@ def clear_line():
 
 
 # Function to generate the board with coordinates and labels
-def generate_board_with_coordinates(board):
+def generate_coordinates(board):
     header = [""]
     header += [chr(ord('A') + i) for i in range(board_size[1])]
     header += [""]
@@ -71,19 +78,15 @@ def generate_board_with_coordinates(board):
 
 
 # Function to print the game boards with coordinates and labels
-def print_boards_with_coordinates(player_board, computer_board):
-    clear_terminal()
-    player_board_with_coordinates = generate_board_with_coordinates(player_board)
-    computer_board_with_coordinates = generate_board_with_coordinates(computer_board)
+def print_coordinates(player_board, computer_board):
+    player_coordinates = generate_coordinates(player_board)
+    computer_coordinates = generate_coordinates(computer_board)
 
     print("Player's Grid:")
-    print(tabulate(player_board_with_coordinates, tablefmt="grid"))
-
-    # Add a gap between player and computer grids
-    print("\n" + " " * 6 + "+--" + "--+" * board_size[1] + "\n")
+    print(tabulate(player_coordinates, tablefmt="grid"))
 
     print("Computer's Grid:")
-    print(tabulate(computer_board_with_coordinates, tablefmt="grid"))
+    print(tabulate(computer_coordinates, tablefmt="grid"))
 
 
 # Function to get the user's guess (row and column)
@@ -91,12 +94,24 @@ def get_user_guess():
     while True:
         try:
             guess = input(f"Enter your guess (e.g., A1, max {chr(ord('A') + board_size[1] - 1)}{board_size[0]}): ")
+
+            if (guess == "restart"):
+
+                clear_terminal()
+                time.sleep(1)
+                play_battleships()
+
             col_row = guess.upper()
+
             # Convert column letter to zero-based index
             col = ord(col_row[0]) - ord('A')
+
             # Subtract 1 to convert from 1-based to 0-based index
             row = int(col_row[1:]) - 1
             if 0 <= col < board_size[1] and 0 <= row < board_size[0]:
+
+                clear_terminal()
+                time.sleep(1)
                 return row, col
             else:
                 print("Invalid input. Please enter valid row and column values.")
@@ -113,48 +128,53 @@ def check_guess(row, col, target_ship):
 
 # Main game loop
 def play_battleships():
-    print_message("Welcome to Battleships!", GREEN)
-
     while True:
-        clear_terminal()
-        # Print both game boards with coordinates and labels
-        print_boards_with_coordinates(player_board, computer_board)
+        # Initialize the player and computer boards
+        player_board, computer_board = reset_board()
 
-        # Player's turn
-        player_row, player_col = get_user_guess()
-        if check_guess(player_row, player_col, computer_ship):
-            print_message("Congratulations! You've sunk the computer's battleship!", GREEN)
-            break
-        else:
-            if player_board[player_row][player_col] == ' ':
+        print_message("Welcome to Battleships!", GREEN)
+
+        while True:
+            # Print both game boards with coordinates and labels
+            print_coordinates(player_board, computer_board)
+
+            # Player's turn
+            player_row, player_col = get_user_guess()
+
+            if check_guess(player_row, player_col, computer_ship):
+                print_message("Congratulations! You've sunk the computer's battleship!", GREEN)
+                break
+
+            elif (player_board[player_row][player_col] == ' '):
                 print_message("You missed. Try again.", RED)
                 player_board[player_row][player_col] = 'X'
+
             else:
                 print_message("You already guessed this location.", RED)
 
-        time.sleep(1)  # Pause for a moment
+            # Clear the previous board display
+            for _ in range(board_size[0] + 5):
+                clear_line()
+                sys.stdout.write("\033[A")
 
-        # Clear the previous board display
-        for _ in range(board_size[0] + 5):
-            clear_line()
-            sys.stdout.write("\033[A")
+            # Computer's turn
+            computer_row, computer_col = random.randint(0, board_size[0] - 1), random.randint(0, board_size[1] - 1)
+            print_message(f"The computer takes a guess at position: {chr(ord('A') + computer_row)} {computer_col + 1}", GREEN)
 
-        # Computer's turn
-        computer_row, computer_col = random.randint(0, board_size[0] - 1), random.randint(0, board_size[1] - 1)
-        print_message(f"The computer takes a guess at position: {chr(ord('A') + computer_row)} {computer_col + 1}", GREEN)
+            if check_guess(computer_row, computer_col, player_ship):
+                print_message("The computer has sunk your battleship! You lose.", RED)
+                break
 
-        if check_guess(computer_row, computer_col, player_ship):
-            print_message("The computer has sunk your battleship! You lose.", RED)
-            break
-        else:
-            if computer_board[computer_row][computer_col] == ' ':
+            elif (computer_board[computer_row][computer_col] == ' '):
                 print_message("The computer missed.", RED)
                 computer_board[computer_row][computer_col] = 'X'
+
             else:
                 print_message("The computer already guessed this location.", RED)
 
-    time.sleep(1)  # Pause for a moment
-
+        restart = input("Do you want to play again? (yes/no): ").strip().lower()
+        if restart != 'yes':
+            break
 
 # Start the game if this script is run as the main program
 if __name__ == "__main__":
